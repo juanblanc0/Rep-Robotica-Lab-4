2e2eca887d63e2da8c41592f6846c79ef623d0e7
Juan Camilo Blanco Barrera <144705516+juanblanc0@users.noreply.github.com>
2025-10-23 23:28:00 -0500

    Refactor motor control and ADC pin assignments

--- main.cpp
+++ main.cpp
@@ -10,39 +10,46 @@ hw_timer_t *timerMuestreo = NULL;
 volatile bool cambiarSentido = false;
 volatile bool muestrear = false;
 static bool sentido = true;
-bool activar = false;   // 🔥 NUEVA BANDERA: habilita el robot
-
-#define PWMA_X 15
-#define AIN1_X 2
-#define AIN2_X 4
+bool activar = false;   // Bandera habilita el robot
 
 
-#define BIN1_X 16
-#define BIN2_X 17
-#define PWMB_X 5
-#define STBY_X 21
 
-/*
-#define AIN1_Y 4
-#define AIN2_Y 2
-#define PWMA_Y 15
-#define BIN1_Y 19
-#define BIN2_Y 18
-#define PWMB_Y 5
-#define STBY_Y 21
-*/
+// Pines ADC1 (solo entrada)
+#define PIN_VN   39   // SENSOR_VN  → ADC1_CH3
+#define PIN_34   34   // GPIO34     → ADC1_CH6
+#define PIN_35   35   // GPIO35     → ADC1_CH7
+#define PIN_32   32   // GPIO32     → ADC1_CH4
 
-const int pinADC1 = 34;
-const int pinADC2 = 35;
-const int pinADC3 = 32;
+
+// Pines 1DOF(Base), 2DOF
+#define PWMA_X 21
+#define AIN1_X 19
+#define AIN2_X 18
+#define BIN3_X 17
+#define BIN4_X 16
+#define PWMB_X 4
+
+// Pines 3DOF , 4DOF
+#define PWMA_Y 33
+#define AIN1_Y 25
+#define AIN2_Y 26
+#define BIN3_Y 27
+#define BIN4_Y 14
+#define PWMB_Y 13
+
+
 
 int raw1 = 0;
 int raw2 = 0;
 int raw3 = 0;
+int raw4 = 0;
+
 
 float vt1 = 0;
 float vt2 = 0;
 float vt3 = 0;
+float vt4 = 0;
+
 
 float referencia = 8.75;
 float angulo = 0;
@@ -55,7 +62,6 @@ float pwbt = 0;
 String inputString = "";
 
 
-// --------- FUNCIONES OMITIDAS POR BREVIDAD ---------
 
 //estos dos van con x osea un puente H
 void movermotor1DE(float pw)
@@ -74,6 +80,8 @@ void movermotor1IZ(float pw)
   analogWrite(PWMA_X,pw);
 }
 
+///falta mover sentido contrario 
+
 void movermotor1STOP(float pw)
 {
   // --- Motor 1 --- (BASE)
@@ -82,29 +90,31 @@ void movermotor1STOP(float pw)
   analogWrite(PWMA_X,pw);
 }
 
-void movermotor2STOP(float pw)
-{
-  // --- Motor 2 --- (CODO)
-  digitalWrite(BIN1_X, LOW);
-  digitalWrite(BIN2_X, LOW);
-  analogWrite(PWMB_X,pw);
-}
-
 void movermotor2DE(float pw)
 {
   // --- Motor 2 --- (CODO)
-  digitalWrite(BIN1_X, LOW);
-  digitalWrite(BIN2_X, HIGH);
+  digitalWrite(BIN3_X, LOW);
+  digitalWrite(BIN4_X, HIGH);
   analogWrite(PWMB_X,pw);
 }
 
 void movermotor2IZ(float pw)
 {
   // --- Motor 2 --- (CODO)
-  digitalWrite(BIN1_X, HIGH);
-  digitalWrite(BIN2_X, LOW);
+  digitalWrite(BIN3_X, HIGH);
+  digitalWrite(BIN4_X, LOW);
   analogWrite(PWMB_X,pw);
 }
+
+void movermotor2STOP(float pw)
+{
+  // --- Motor 2 --- (CODO)
+  digitalWrite(BIN3_X, LOW);
+  digitalWrite(BIN4_X, LOW);
+  analogWrite(PWMB_X,pw);
+}
+
+
 
 // --------- TIMERS ISR ---------
 void IRAM_ATTR onTimerGiro() { cambiarSentido = true; }
@@ -132,6 +142,7 @@ void loopBlutuchi(void *parameter){
 // --------- TAREA CONTROL ---------
 void loopControl(void *parameter){
   for(;;){
+
     if (inputString == "0,1") { 
       // Si no está activado, mantener motores apagados
       movermotor1DE(255);
@@ -152,14 +163,16 @@ void loopControl(void *parameter){
     if (muestrear) {
       
       muestrear = false;
-      raw1= analogRead(pinADC1);
-      raw2= analogRead(pinADC2);
-      raw3= analogRead(pinADC3);
+      raw1= analogRead(PIN_VN);
+      raw2= analogRead(PIN_34);
+      raw3= analogRead(PIN_VN);
+      raw4= analogRead(PIN_34);
 
       vt1 = (raw1 / 4095.0) * 3.3;
       vt2 = (raw2 / 4095.0) * 3.3;
       vt3 = (raw3 / 4095.0) * 3.3;
-      
+      vt4 = (raw4 / 4095.0) * 3.3;
+
     }
 
     
@@ -178,8 +191,10 @@ void setup() {
   xTaskCreatePinnedToCore(loopControl,"TaskControl",4096,NULL,1,&TaskControl,1);
 
   pinMode(AIN1_X, OUTPUT); pinMode(AIN2_X, OUTPUT); pinMode(PWMA_X, OUTPUT);
-  pinMode(BIN1_X, OUTPUT); pinMode(BIN2_X, OUTPUT); pinMode(PWMB_X, OUTPUT);
-  pinMode(STBY_X, OUTPUT);
+  pinMode(BIN3_X, OUTPUT); pinMode(BIN4_X, OUTPUT); pinMode(PWMB_X, OUTPUT);
+
+  pinMode(AIN1_Y, OUTPUT); pinMode(AIN2_Y, OUTPUT); pinMode(PWMA_Y, OUTPUT);
+  pinMode(BIN3_Y, OUTPUT); pinMode(BIN4_Y, OUTPUT); pinMode(PWMB_Y, OUTPUT);
 
   timerGiro = timerBegin(0, 80, true);
   timerAttachInterrupt(timerGiro, &onTimerGiro, true);
@@ -191,7 +206,7 @@ void setup() {
   timerAlarmWrite(timerMuestreo, 2000, true); // 2ms
   timerAlarmEnable(timerMuestreo);
 
-  digitalWrite(STBY_X, HIGH);
+
 }
 
 void loop() {}
